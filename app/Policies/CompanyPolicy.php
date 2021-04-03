bc4bdff6174e696fb56bf48b3d5ff8fed69384e8
El Strategico <strategico.dev@gmail.com>
2021-04-04 03:09:30 +0800

    Added CompanyPolicy

---
+++ app/Policies/CompanyPolicy.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use App\Models\Company;
+
+class CompanyPolicy
+{
+    /**
+     * @param User $user
+     * @param Company $company
+     * @return bool
+     */
+    public function show(User $user, Company $company)
+    {
+        return $user->id === $company->user_id;
+    }
+
+    /**
+     * @param User $user
+     * @param Company $company
+     * @return bool
+     */
+    public function update(User $user, Company $company)
+    {
+        return $user->id === $company->user_id;
+    }
+}
