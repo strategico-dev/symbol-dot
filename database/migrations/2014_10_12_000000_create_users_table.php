f316751ac39c5e6180da3a270fe84c743b9a25a5
El Strategico <strategico.dev@gmail.com>
2021-04-04 03:06:28 +0800

    Refactoring

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('avatar')->default('/uploads/avatars/default.png');
             $table->timestamps();
         });
     }
