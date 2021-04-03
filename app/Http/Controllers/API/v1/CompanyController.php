00a39cc09d1a2c55a1ec65d9a09814fcf36fd6ea
El Strategico <strategico.dev@gmail.com>
2021-04-04 05:59:36 +0800

    Added methods: update, delete

--- app/Http/Controllers/API/v1/CompanyController.php
+++ app/Http/Controllers/API/v1/CompanyController.php
@@ -4,8 +4,11 @@ namespace App\Http\Controllers\API\v1;
 
 use App\Models\User;
 use App\Models\Company;
-use Illuminate\Http\Request;
+use App\Models\CompanyDetail;
+use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\CreateCompanyRequest;
+use App\Http\Requests\UpdateCompanyRequest;
 
 class CompanyController extends Controller
 {
@@ -18,10 +21,10 @@ class CompanyController extends Controller
     }
 
     #Route::post('/api/v1/companies')
-    public function store(Request $request)
+    public function store(CreateCompanyRequest $request)
     {
         return Company::create(
-            $request->input('company'),
+            $request->input(),
             $request->input('detail')
         );
     }
@@ -32,6 +35,40 @@ class CompanyController extends Controller
         $company = Company::with(['companyDetail'])->findOrFail($companyId);
         $this->authorize('show', $company);
 
+        return $company;
+    }
+
+    #Route::put('/api/v1/companies/{company}')
+    public function update(Company $company, UpdateCompanyRequest $request)
+    {
+        $this->authorize('update', $company);
+
+        $company->update($request->only(['name', 'description']));
+        if($request->exists('detail'))
+        {
+            if(!$company->companyDetail)
+            {
+                DB::transaction(function () use ($company) {
+                    $companyDetail = CompanyDetail::create($request->input('detail'));
+                    $company->companyDetail()->associate($companyDetail);
+                    $company->save();
+                });
+            }
+            else
+            {
+                $company->companyDetail()->update($request->input('detail'));
+            }
+        }
+
+        return $company;
+    }
+
+    #Route::delete('/api/v1/companies/{company}')
+    public function delete(Company $company)
+    {
+        $this->authorize('delete', $company);
+
+        $company->delete();
         return $company;
     }
 }
