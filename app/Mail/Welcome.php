770d94c57953ba41ed3110b88bc394fb65f2b308
El Strategico <strategico.dev@gmail.com>
2021-04-04 03:12:44 +0800

    Added Welcome mail

---
+++ app/Mail/Welcome.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class Welcome extends Mailable implements ShouldQueue
+{
+    use Queueable, SerializesModels;
+
+    private $user;
+
+    /**
+     * Create a new message instance.
+     * @param mixed $user
+     * @return void
+     */
+    public function __construct($user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->subject('Добро пожаловать!')->
+                      view('emails.welcome', ['user' => $this->user]);
+    }
+}
