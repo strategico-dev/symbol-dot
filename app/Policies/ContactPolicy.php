fdf1d0cb6925ed3183f8108db6188ab85f4f0a08
El Strategico <strategico.dev@gmail.com>
2021-04-04 03:09:20 +0800

    Added ContactPolicy

---
+++ app/Policies/ContactPolicy.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use App\Models\Contact;
+use App\Models\ContactPermission;
+
+class ContactPolicy
+{
+    /**
+     * @param User $user
+     * @param Contact $contact
+     * @return bool
+     */
+    public function show(User $user, Contact $contact)
+    {
+        if($user->id === $contact->user_id || ContactPermission::isAdmin($user, $contact))
+        {
+            return true;
+        }
+
+        return ContactPermission::canRead($user, $contact);
+    }
+
+    /**
+     * @param User $user
+     * @param Contact $contact
+     * @return bool
+     */
+    public function update(User $user, Contact $contact)
+    {
+        if($user->id === $contact->user_id || ContactPermission::isAdmin($user, $contact))
+        {
+            return true;
+        }
+
+        return ContactPermission::canWrite($user, $contact);
+    }
+
+    /**
+     * @param User $user
+     * @param Contact $contact
+     * @return bool
+     */
+    public function changePermission(User $user, Contact $contact)
+    {
+        return $user->id === $contact->user_id || ContactPermission::isAdmin($user, $contact);
+    }
+}
