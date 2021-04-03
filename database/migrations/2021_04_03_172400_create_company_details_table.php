7ac73bb098da0df05e683fca045fc1e96c5b9cc3
El Strategico <strategico.dev@gmail.com>
2021-04-04 03:06:01 +0800

    Added migration for company_details table

---
+++ database/migrations/2021_04_03_172400_create_company_details_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompanyDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('company_details', function (Blueprint $table) {
+            $table->id();
+            $table->string('ogrn')->nullable();
+            $table->string('inn')->nullable();
+            $table->string('kpp')->nullable();
+            $table->string('address')->nullable();
+            $table->integer('authorized_capital')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('company_details');
+    }
+}
