85cc365ca4ee1fe6755fc73e8d55d3be4fdfbef2
El Strategico <strategico.dev@gmail.com>
2021-04-04 03:05:43 +0800

    Added migration for contact_permissions table

---
+++ database/migrations/2021_04_03_124653_create_contact_permissions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\ContactPermission;
+
+class CreateContactPermissionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contact_permissions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedBigInteger('contact_id');
+            $table->foreign('contact_id')->references('id')->on('contacts');
+            $table->integer('mode')->default(ContactPermission::READ_MODE);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contact_permissions');
+    }
+}
