686e3cf846f8d836457a929aa02e4c6a4d7131b6
El Strategico <strategico.dev@gmail.com>
2021-04-04 03:11:06 +0800

    Added emails views

---
+++ resources/views/emails/created-contact.blade.php
@@ -0,0 +1,6 @@
+<div>
+    <h1>Вы создали новый контакт</h1>
+    <div>
+        <span>Здравствуйте, вы создали новый контакт с именем {{$contact->first_name}} в Symbol Dot CRM</span>
+    </div>
+</div>
---
+++ resources/views/emails/welcome.blade.php
@@ -0,0 +1,14 @@
+<div>
+    <h1>Вы зарегистрировались в Symbol Dot CRM!</h1>
+    <div>
+        <span>Здравствуйте, {{$user->name}}!</span>
+    </div>
+    <div>
+        <div>
+            <span>Symbol Dot CRM - Open source CRM система, для вашего бизнеса!</span>
+        </div>
+        <div>
+            <span>Symbol Dot - позволяет вам вести учет клиентов, формировать воронки продаж, вести бухгалтерский учет и многое другое.</span>
+        </div>
+    </div>
+</div>
