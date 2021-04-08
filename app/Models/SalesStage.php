8cad85c43a4450f8c3973a81f3728c222d482392
El Strategico <strategico.dev@gmail.com>
2021-04-08 16:41:50 +0800

    Added SalesStage model

---
+++ app/Models/SalesStage.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Models;
+
+use App\Observers\SalesStageObserver;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\DB;
+
+/**
+ * @property int $order
+ * @property int $sales_funnel_id
+ */
+class SalesStage extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'order',
+        'sales_funnel_id'
+    ];
+
+    public function contacts()
+    {
+        return $this->belongsToMany(Contact::class, 'contact_stage');
+    }
+
+    /**
+     * @param SalesStage $place
+     * @param SalesStage $target
+     */
+    private static function swapOrder(SalesStage $place, SalesStage $target)
+    {
+        $placeOrder = $place->order;
+        $place->order = $target->order;
+        $target->order = $placeOrder;
+
+        $place->save();
+        $target->save();
+    }
+
+    #TODO: Refactoring
+    /**
+     * @param SalesFunnel $salesFunnel
+     * @param array $swappable
+     */
+    public static function swapOrders(SalesFunnel $salesFunnel, array $swappable)
+    {
+        foreach($swappable as $placeId => $targetId)
+        {
+            /* @var SalesStage $place */
+            $place = self::where('sales_funnel_id', $salesFunnel->id)->findOrFail($placeId);
+            /* @var SalesStage $target */
+            $target = self::where('sales_funnel_id', $salesFunnel->id)->findOrFail($targetId);
+
+            self::swapOrder($place, $target);
+        }
+    }
+
+    public static function moveTo(SalesStage $current, SalesStage $target, Contact $contact)
+    {
+        DB::transaction(function () use ($current, $target, $contact) {
+            $current->contacts()->detach($contact);
+            $target->contacts()->attach($contact);
+        });
+    }
+
+    protected static function boot()
+    {
+        parent::boot();
+        self::observe(SalesStageObserver::class);
+    }
+}
