44bfe4ba5b23d96450866fc6e5db78617af01587
El Strategico <strategico.dev@gmail.com>
2021-04-08 16:40:35 +0800

    Added migration for sales_stages table

---
+++ database/migrations/2021_04_08_060920_create_sales_stages_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSalesStagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sales_stages', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->integer('order')->default(0);
+            $table->unsignedBigInteger('sales_funnel_id');
+            $table->foreign('sales_funnel_id')->references('id')->on('sales_funnels');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sales_stages');
+    }
+}
