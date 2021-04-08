7df5c102eb0127850a7ea3a4def2428eeafc26bc
El Strategico <strategico.dev@gmail.com>
2021-04-08 16:42:54 +0800

    Added SalesFunnelController

---
+++ app/Http/Controllers/API/v1/SalesFunnelController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\API\v1;
+
+use App\Models\User;
+use App\Models\SalesFunnel;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\CreateSalesFunnelRequest;
+
+class SalesFunnelController extends Controller
+{
+    #Route::get('/api/v1/sales-funnels')
+    public function index()
+    {
+        /* @var User $user */
+        $user = User::find(auth()->id());
+        return $user->salesFunnels()->fetch();
+    }
+
+    #Route::post('/api/v1/sales-funnels')
+    public function store(CreateSalesFunnelRequest $request)
+    {
+        /* @var User $user */
+        $user = User::find(auth()->id());
+        return $user->salesFunnels()->save(new SalesFunnel($request->input()));
+    }
+
+    #Route::get('/api/v1/sales-funnels/{salesFunnelId}')
+    public function show($salesFunnelId)
+    {
+        $salesFunnel = SalesFunnel::with(['salesStages'])->
+                                    findOrFail($salesFunnelId);
+
+        $this->authorize('show', $salesFunnel);
+
+        return $salesFunnel;
+    }
+
+    #Route::delete('/api/v1/sales-funnels/{salesFunnel}')
+    public function delete(SalesFunnel $salesFunnel)
+    {
+        $this->authorize('delete', $salesFunnel);
+
+        $salesFunnel->delete();
+        return $salesFunnel;
+    }
+}
